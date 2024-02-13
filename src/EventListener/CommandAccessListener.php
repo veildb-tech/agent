19bc6fe5f8fbb0c980505c256eaba31aa3882989
Koblia Serhii <sergiy.k@overdose.digital>
2024-02-13 19:24:23 +0200

    [325] Changelog:
    Changed:
    
    - hide update command in case UUID empty
    - added additioanl checking for UUID

---
+++ src/EventListener/CommandAccessListener.php
@@ -0,0 +1,45 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\EventListener;
+
+use App\Service\AppConfig;
+use Symfony\Component\Console\ConsoleEvents;
+use Symfony\Component\Console\Event\ConsoleCommandEvent;
+use Symfony\Component\EventDispatcher\EventSubscriberInterface;
+
+readonly class CommandAccessListener implements EventSubscriberInterface
+{
+    public function __construct(
+        private AppConfig $appConfig
+    ) {
+    }
+
+    /**
+     * @param ConsoleCommandEvent $event
+     *
+     * @return void
+     */
+    public function onConsoleCommand(ConsoleCommandEvent $event): void
+    {
+        $command = $event->getCommand();
+
+        if ($command->getName() === 'app:server:update' && !$this->appConfig->getServerUuid()) {
+            $command->setHidden(true);
+            $event->disableCommand();
+
+            $output = $event->getOutput();
+            $output->writeln(
+                '<error>An information about the server did\'t found. You must to use app:server:add to add the server.</error>'
+            );
+        }
+    }
+
+    public static function getSubscribedEvents(): array
+    {
+        return [
+            ConsoleEvents::COMMAND => 'onConsoleCommand',
+        ];
+    }
+}
